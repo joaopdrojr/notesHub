f655705e3a199a6e5d382400e1bb8b30d1e69b83
joao.pdro <joao.pdrojr@gmail.com>
2025-12-21 23:40:01 -0300

    Initial Commit

---
+++ notesHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notesHub
+//
+//  Created by Joao Pedro Junior on 21/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notesHub/notesHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesHubApp.swift
+//  notesHub
+//
+//  Created by Joao Pedro Junior on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct notesHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
